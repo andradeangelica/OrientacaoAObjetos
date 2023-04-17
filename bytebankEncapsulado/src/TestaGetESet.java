59ebe26185630db2bb148aa0cc78ff626bf66608
Angélica Alves Andrade <102556999+andradeangelica@users.noreply.github.com>
2023-04-17 16:36:16 -0300

    Add files via upload

---
+++ bytebankEncapsulado/src/TestaGetESet.java
@@ -0,0 +1,20 @@
+
+public class TestaGetESet {
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta(1337, 24226);
+		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		//conta.titular = paulo;
+		
+		paulo.setNome("paulo silveira");
+		
+		conta.setTitular(paulo);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programador");
+	}
+
+}
